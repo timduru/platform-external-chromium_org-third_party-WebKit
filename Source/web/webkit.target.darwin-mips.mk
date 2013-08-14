00d3faa021f1e9934a353eeebe4e57e354856cf6
Ben Murdoch <benm@google.com>
2013-08-14 11:52:03 +0100

    Merge from Chromium at DEPS revision r217147
    This commit was generated by merge_to_master.py.
    
    Change-Id: Icd67b02e5f1ff621b8d2455a6e7e4eb98aba6900

--- Source/web/webkit.target.darwin-mips.mk
+++ Source/web/webkit.target.darwin-mips.mk
@@ -383,7 +383,6 @@ LOCAL_C_INCLUDES_Debug := \
 	$(LOCAL_PATH)/third_party/skia/include/effects \
 	$(LOCAL_PATH)/third_party/skia/include/pdf \
 	$(LOCAL_PATH)/third_party/skia/include/gpu \
-	$(LOCAL_PATH)/third_party/skia/include/gpu/gl \
 	$(LOCAL_PATH)/third_party/skia/include/lazy \
 	$(LOCAL_PATH)/third_party/skia/include/pathops \
 	$(LOCAL_PATH)/third_party/skia/include/pipe \
@@ -532,7 +531,6 @@ LOCAL_C_INCLUDES_Release := \
 	$(LOCAL_PATH)/third_party/skia/include/effects \
 	$(LOCAL_PATH)/third_party/skia/include/pdf \
 	$(LOCAL_PATH)/third_party/skia/include/gpu \
-	$(LOCAL_PATH)/third_party/skia/include/gpu/gl \
 	$(LOCAL_PATH)/third_party/skia/include/lazy \
 	$(LOCAL_PATH)/third_party/skia/include/pathops \
 	$(LOCAL_PATH)/third_party/skia/include/pipe \
