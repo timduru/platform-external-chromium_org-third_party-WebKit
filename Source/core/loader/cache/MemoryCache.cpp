d28864f1df4b18a34caa30ef241d34f98112b921
Torne (Richard Coles) <torne@google.com>
2013-08-21 10:31:59 +0100

    Merge from Chromium at DEPS revision 30.0.1599.14
    This commit was generated by merge_to_master.py.
    
    Change-Id: I76741d74711fcd63604b9be8c8927b6c1f234967

--- Source/core/loader/cache/MemoryCache.cpp
+++ Source/core/loader/cache/MemoryCache.cpp
@@ -30,7 +30,6 @@
 #include "core/loader/cache/ResourcePtr.h"
 #include "core/page/FrameView.h"
 #include "core/platform/Logging.h"
-#include "core/platform/chromium/TraceEvent.h"
 #include "core/workers/WorkerGlobalScope.h"
 #include "core/workers/WorkerLoaderProxy.h"
 #include "core/workers/WorkerThread.h"
@@ -560,7 +559,6 @@ void MemoryCache::evictResources()
 
 void MemoryCache::prune()
 {
-    TRACE_EVENT0("renderer", "MemoryCache::prune()");
     if (m_liveSize + m_deadSize <= m_capacity && m_maxDeadCapacity && m_deadSize <= m_maxDeadCapacity) // Fast path.
         return;
     if (m_inPruneResources)
